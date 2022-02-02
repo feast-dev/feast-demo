f8ce4b0a4040ccc0387a4e6e5f85780ff9685235
Danny Chiao <danny@tecton.ai>
2022-02-02 18:06:48 -0500

    add grpc call to local server
    Signed-off-by: Danny Chiao <danny@tecton.ai>

--- feature_repo/features.py
+++ feature_repo/features.py
@@ -1,10 +1,9 @@
-from google.protobuf.duration_pb2 import Duration
-
+import pandas as pd
 from feast import Entity, Feature, FeatureView, FileSource, ValueType
 from feast.data_source import RequestDataSource
 from feast.on_demand_feature_view import on_demand_feature_view
 from feast.request_feature_view import RequestFeatureView
-import pandas as pd
+from google.protobuf.duration_pb2 import Duration
 
 driver_hourly_stats = FileSource(
     path="data/driver_stats_with_string.parquet",
@@ -27,32 +26,29 @@ driver_hourly_stats_view = FeatureView(
     tags={},
 )
 
-# Define a request data source which encodes features / information only 
+# Define a request data source which encodes features / information only
 # available at request time (e.g. part of the user initiated HTTP request)
 input_request = RequestDataSource(
     name="vals_to_add",
-    schema={
-        "val_to_add": ValueType.INT64,
-        "val_to_add_2": ValueType.INT64
-    }
+    schema={"val_to_add": ValueType.INT64, "val_to_add_2": ValueType.INT64},
 )
 
-# Define an on demand feature view which can generate new features based on 
+# Define an on demand feature view which can generate new features based on
 # existing feature views and RequestDataSource features
 @on_demand_feature_view(
-   inputs={
-       'driver_hourly_stats': driver_hourly_stats_view,
-       'vals_to_add': input_request
-   },
-   features=[
-     Feature(name='conv_rate_plus_val1', dtype=ValueType.DOUBLE),
-     Feature(name='conv_rate_plus_val2', dtype=ValueType.DOUBLE)
-   ]
+    inputs={
+        "driver_hourly_stats": driver_hourly_stats_view,
+        "vals_to_add": input_request,
+    },
+    features=[
+        Feature(name="conv_rate_plus_val1", dtype=ValueType.DOUBLE),
+        Feature(name="conv_rate_plus_val2", dtype=ValueType.DOUBLE),
+    ],
 )
 def transformed_conv_rate(inputs: pd.DataFrame) -> pd.DataFrame:
     df = pd.DataFrame()
-    df['conv_rate_plus_val1'] = (inputs['conv_rate'] + inputs['val_to_add'])
-    df['conv_rate_plus_val2'] = (inputs['conv_rate'] + inputs['val_to_add_2'])
+    df["conv_rate_plus_val1"] = inputs["conv_rate"] + inputs["val_to_add"]
+    df["conv_rate_plus_val2"] = inputs["conv_rate"] + inputs["val_to_add_2"]
     return df
 
 
@@ -60,9 +56,6 @@ def transformed_conv_rate(inputs: pd.DataFrame) -> pd.DataFrame:
 driver_age_request_fv = RequestFeatureView(
     name="driver_age",
     request_data_source=RequestDataSource(
-        name="driver_age",
-        schema={
-            "driver_age": ValueType.INT64,
-        }
-    )
+        name="driver_age", schema={"driver_age": ValueType.INT64,}
+    ),
 )
--- feature_repo/get_features_demo.py
+++ feature_repo/get_features_demo.py
@@ -1,6 +1,35 @@
-from feast import FeatureStore
 from datetime import datetime
+
+import grpc
 import pandas as pd
+from feast import FeatureStore
+from feast.protos.feast.serving.ServingService_pb2 import (
+    FeatureList,
+    FeatureReferenceV2,
+    GetOnlineFeaturesRequest,
+    GetOnlineFeaturesRequestV2,
+)
+from feast.protos.feast.serving.ServingService_pb2_grpc import ServingServiceStub
+from feast.protos.feast.types.Value_pb2 import RepeatedValue, Value
+
+
+# Sample logic to fetch from a local gRPC java server deployed at 6566
+def fetch_java():
+    channel = grpc.insecure_channel("localhost:6566")
+    stub = ServingServiceStub(channel)
+    feature_refs = FeatureList(val=["driver_hourly_stats:conv_rate"])
+    entity_rows = {
+        "driver_id": RepeatedValue(
+            val=[Value(int64_val=driver_id) for driver_id in range(1001, 1003)]
+        )
+    }
+
+    print(
+        stub.GetOnlineFeatures(
+            GetOnlineFeaturesRequest(features=feature_refs, entities=entity_rows,)
+        )
+    )
+
 
 def run_demo():
     store = FeatureStore(repo_path=".")
@@ -29,7 +58,7 @@ def run_demo():
             "driver_hourly_stats:avg_daily_trips",
             "transformed_conv_rate:conv_rate_plus_val1",
             "transformed_conv_rate:conv_rate_plus_val2",
-            "driver_age:driver_age"
+            "driver_age:driver_age",
         ],
     ).to_df()
     print(training_df.head())
@@ -42,28 +71,30 @@ def run_demo():
             "driver_hourly_stats:avg_daily_trips",
             "transformed_conv_rate:conv_rate_plus_val1",
             "transformed_conv_rate:conv_rate_plus_val2",
-            "driver_age:driver_age"
+            "driver_age:driver_age",
         ],
-        entity_rows=[{"driver_id": 1001, "val_to_add": 1000, "val_to_add_2": 2000, "driver_age": 25}],
+        entity_rows=[
+            {
+                "driver_id": 1001,
+                "val_to_add": 1000,
+                "val_to_add_2": 2000,
+                "driver_age": 25,
+            }
+        ],
     ).to_dict()
     for key, value in sorted(features.items()):
-        print(key, ' : ', value)
-
+        print(key, " : ", value)
 
     print("\n--- Simulate a stream event ingestion of the hourly stats df ---")
     event_df = pd.DataFrame.from_dict(
         {
             "driver_id": [1001],
-            "event_timestamp": [
-                datetime(2021, 5, 13, 10, 59, 42),
-            ],
-            "created": [
-                datetime(2021, 5, 13, 10, 59, 42),
-            ],
+            "event_timestamp": [datetime(2021, 5, 13, 10, 59, 42),],
+            "created": [datetime(2021, 5, 13, 10, 59, 42),],
             "conv_rate": [1.0],
             "acc_rate": [1.0],
             "avg_daily_trips": [1000],
-            "string_feature": "test2"
+            "string_feature": "test2",
         }
     )
     print(event_df)
@@ -76,12 +107,19 @@ def run_demo():
             "driver_hourly_stats:avg_daily_trips",
             "transformed_conv_rate:conv_rate_plus_val1",
             "transformed_conv_rate:conv_rate_plus_val2",
-            "driver_age:driver_age"
+            "driver_age:driver_age",
         ],
-        entity_rows=[{"driver_id": 1001, "val_to_add": 1000, "val_to_add_2": 2000, "driver_age": 25}],
+        entity_rows=[
+            {
+                "driver_id": 1001,
+                "val_to_add": 1000,
+                "val_to_add_2": 2000,
+                "driver_age": 25,
+            }
+        ],
     ).to_dict()
     for key, value in sorted(features.items()):
-        print(key, ' : ', value)
+        print(key, " : ", value)
 
 
 if __name__ == "__main__":
